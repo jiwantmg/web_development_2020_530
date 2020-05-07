f6b04b9a8a4a6dbef71e2197e00ac6afd9ec0690
kiran <lungelikiran02@gmail.com>
2020-05-07 12:13:30 -0400

    My secound assignment modified

--- Assignments/Kiran/JavaScript/secoundassignment/assignment.js
+++ Assignments/Kiran/JavaScript/secoundassignment/assignment.js
@@ -11,36 +11,48 @@ import quesTen from './qten.js';
 import quesEleven from './qeleven.js';
 import quesFive from './qfive.js';
 
+document.getElementById("clickMe").onclick = function(){
+    let num = document.getElementById("myText").value;
+    myFunction(num);
+};
+function myFunction(x){
+    console.clear();
+    switch(x){
+        case '1':
+            quesOne();
+            break;
+        case '2':
+            quesTwo();
+            break;
+        case '3':
+            quesThree();
+            break;
+        case '4':
+            quseFour();
+            break;
+        case '5':
+            quesFive();
+            break;
+        case '6':
+            quesSix();
+            break;
+        case '7':
+            quseSeven();
+            break;
+        case '8':
+            quesEight();
+            break;
+        case '9':
+            quesNine();
+            break;
+        case '10':
+            quesTen();
+            break;
+        case '11':
+            quesEleven();
+            break;
+    };
 
-console.log("Question number 1");
-    quesOne();
 
-console.log("Question number 2");
-    quesTwo();
-
-console.log("Question number 3");
-    quesThree();
-
-console.log("Question number 4");
-    quseFour();
-
-console.log("Question number 5");
-   quesFive();
-
-console.log("Question number 6");
-    quesSix();
+};
     
-console.log("Question number 7");
-    quseSeven();
-
-console.log("Question number 8");
-    quesEight();
-
-console.log("Question number 9");
-    quesNine();
-
-console.log("Question number 10");
-    quesTen();
-
-console.log("Question number 11");
-    quesEleven();
